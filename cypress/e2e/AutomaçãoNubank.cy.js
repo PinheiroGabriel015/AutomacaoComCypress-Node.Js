73017eaa6a32f6284ca0140fb690e09a7651e807
PinheiroGabriel015 <gabrielpc1307@gmail.com>
2025-03-31 09:09:43 -0300

    agora vai

--- cypress/e2e/AutomaçãoNubank.cy.js
+++ cypress/e2e/AutomaçãoNubank.cy.js
@@ -1,18 +1,23 @@
 /// <reference types='cypress'/>
 describe('Teste Nubank', ()=>{
     it('Teste de login', function(){
+        cy.readFile('cypress/fixtures/dadosFalsos.json').then((dados) =>{
+            const{nome, cpf, tel, nasc, email} = dados
+
+        
         cy.visit('https://www.nubank.com.br/')
         cy.wait(5000)
         cy.get('.css-wl6yh0').click()
-        cy.get('.chakra-stack > :nth-child(1) > .chakra-input__group > #cpf').type('54161043848')
-        cy.get('#name').type('Gabriel Flash code')
-        cy.get('#phone').type('11999948888')
-        cy.get('#email').type('gabrielflashcodetest@gmail.com')
-        cy.get('#emailConfirmation').type('gabrielflashcodetest@gmail.com')
+        cy.get('.chakra-stack > :nth-child(1) > .chakra-input__group > #cpf').type(cpf)
+        cy.get('#name').type(nome)
+        cy.get('#phone').type('6'+ tel)
+        cy.get('#email').type(email)
+        cy.get('#emailConfirmation').type(email)
         cy.get('.chakra-checkbox__control').click()
         })
+    })
 
-        it('Login com erros', function(){
+        /*it('Login com erros', function(){
             cy.visit('https://www.nubank.com.br/')
             cy.wait(5000)
             cy.get('.css-wl6yh0').click()
@@ -23,5 +28,5 @@ describe('Teste Nubank', ()=>{
             cy.get('#emailConfirmation').type('gabrielflashcodetest@gmai.com')
             cy.get('.chakra-checkbox__control').click()
 
-        })
+        })*/
 })
---
+++ cypress/e2e/RequerendoInfosFalsas.cy.js
@@ -0,0 +1,43 @@
+///<reference types='cypress'/>
+
+describe('Pegando Infos pessoais Falsas para o teste', ()=>{
+    it('Testando comando gpt', function(){
+        cy.visit('https://www.4devs.com.br/gerador_de_pessoas')
+        cy.get('#cookiescript_accept').click()
+        cy.get('#bt_gerar_pessoa').click()
+        cy.wait(2000)
+        cy.get('#nome').should('exist').invoke('text').then((nome)=>{
+            const nomeFalso = nome.trim();
+            
+            cy.get('#cpf > :nth-child(1)').should('exist').invoke('text').then((cpf)=>{
+                const cpfFalso = cpf.trim();
+
+                cy.get('#data_nasc > :nth-child(1)').should('exist').invoke('text').then((nasc)=>{
+                    const nascFalso = nasc.trim();
+
+                    cy.get('#telefone_fixo > :nth-child(1)').should('exist').invoke('text').then((tel)=>{
+                        const telFalso = tel.trim();
+
+                        cy.get('#email > :nth-child(1)').should('exist').invoke('text').then((email)=>{
+                            const emailFalso = email.trim();
+
+
+                              const dadosFalsos ={
+
+                              
+                                nome: nomeFalso,
+                                cpf:cpfFalso,
+                                nasc:nascFalso,
+                                tel:telFalso,
+                                email: emailFalso
+                              }
+                              cy.writeFile('cypress/fixtures/dadosFalsos.json', dadosFalsos);
+                        })
+                    })
+                })
+            })
+        })
+
+
+    })
+})
